14478a58f191f44939df439a349a2493dc41d4e7
Fernando Rojo <frojo@sas.upenn.edu>
2022-05-19 12:24:33 -0400

    broken reanimated

--- App.js
+++ App.js
@@ -1,54 +1,18 @@
-import { AnimatePresence, View } from "moti";
-import { useReducer } from "react";
-import { Pressable, StyleSheet } from "react-native";
-
-function Shape({ bg }) {
-  return (
-    <View
-      from={{
-        opacity: 0,
-        scale: 0.5,
-      }}
-      animate={{
-        opacity: 1,
-        scale: 1,
-      }}
-      exit={{
-        opacity: 0,
-        scale: 0.9,
-      }}
-      style={[styles.shape, { backgroundColor: bg }]}
-    />
-  );
-}
+import Animated, {
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated";
 
 export default function App() {
-  const [visible, toggle] = useReducer((s) => !s, true);
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: withSpring("0%"),
+        },
+      ],
+    };
+  });
 
-  return (
-    <Pressable onPress={toggle} style={styles.container}>
-      <AnimatePresence exitBeforeEnter>
-        {visible && <Shape bg="hotpink" key="hotpink" />}
-        {!visible && <Shape bg="cyan" key="cyan" />}
-      </AnimatePresence>
-    </Pressable>
-  );
+  return <Animated.View style={style} />;
 }
-
-const styles = StyleSheet.create({
-  shape: {
-    justifyContent: "center",
-    height: 250,
-    width: 250,
-    borderRadius: 25,
-    marginRight: 10,
-    backgroundColor: "white",
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    backgroundColor: "#9c1aff",
-  },
-});
